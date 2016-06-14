6ad0250ab8dadf44e696c2a71e19d993d777ef3f
Maaarmotte <loic.habegger@epfl.ch>
2016-06-15 01:54:38 +0200

    Added (not finished) spawning tool

--- lua/weapons/gmod_tool/stools/ls_spawn_tool.lua
+++ lua/weapons/gmod_tool/stools/ls_spawn_tool.lua
@@ -2,23 +2,78 @@ TOOL.Category = "SpaceThing"
 TOOL.Name = "Spawn Tool"
 TOOL.Command = nil
 TOOL.ConfigName = ""
+TOOL.ClientConVar["selected"] = "model/error.mdl"
 
 AddCSLuaFile()
 
+local generators = { ls_generator_solar = "Solar panel",  }
+local storages = { ls_battery = "Battery" }
+
 if CLIENT then
-	language.Add("Tool.sm_link_tool.name", "Spawn  tool")
-	language.Add("Tool.sm_link_tool.desc", "Use it to spawn life support devices.")
-	language.Add("Tool.sm_link_tool.0", "Click to spawn the device.")
+	language.Add("Tool.ls_spawn_tool.name", "Spawn  tool")
+	language.Add("Tool.ls_spawn_tool.desc", "Use it to spawn life support devices.")
+	language.Add("Tool.ls_spawn_tool.0", "Click to spawn the device.")
 	
 	function TOOL.BuildCPanel(CPanel)
-		Cpanel:AddControl("Header", { Text = "Spawn tool", Description = "Left click to spawn a life support device" })
+		SUPERPANEL = CPanel
+		
+		CPanel:AddControl("Header", { Text = "Spawn tool", Description = "Left click to spawn a life support device" })
+		
+		local dtree = vgui.Create("DTree", SUPERPANEL)
+		CPanel:AddPanel(dtree)
+		dtree.OnNodeSelected = function(self, node)
+			if node.path then
+				LocalPlayer():ConCommand("ls_spawn_tool_selected " .. node.path)
+			end
+		end
+
+		local sx, sy = CPanel:GetSize()
+		local xmargin = 15
+		
+		dtree:SetPos(xmargin, 60)
+		dtree:SetSize(sx - 2*xmargin, 240)
+		
+		-- Append generators
+		local gen = dtree:AddNode("Generators")
+		for k,v in pairs(generators) do
+			local node = gen:AddNode(v)
+			node.Icon:SetImage("materials/icon16/cog.png")
+			node.path = k
+		end
+		
+		-- Append storages
+		local stor = dtree:AddNode("Storages")
+		for k,v in pairs(storages) do
+			local node = stor:AddNode(v)
+			node.Icon:SetImage("materials/icon16/database.png")
+			node.path = k
+		end
 	end
 end
 
-function TOOL:LeftClick(tr)
+function TOOL:LeftClick(trace)
+	if trace.Entity && trace.Entity:IsPlayer() then return false end
+	
+	if CLIENT then return true end
+
+	local selected = self:GetClientInfo("selected")
+	if not selected then return end
+		
+	local entity = ents.Create(selected)
+	entity:Spawn()
+	entity:Activate()
+	entity:SetPos(trace.HitPos - Vector(0, 0, entity():OBBMins().z))
+	
+	undo.Create("Life Support")
+		undo.AddEntity(entity)
+	undo.Finish()
+
+	self:GetOwner():AddCleanup("Life Support", entity)
+
+	return true
 end
 
-function TOOL:RightClick(tr)
+function TOOL:RightClick(trace)
 
 end
 
@@ -26,7 +81,7 @@ function TOOL:Think()
 
 end
 
-function TOOL:Reload(tr)
+function TOOL:Reload(trace)
 
 end
 
