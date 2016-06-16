1d2aa1e51bb0b1d98c8142d112911b9a6b18bb1c
Pulp <elowe.n@hotmail.fr>
2016-06-16 17:14:30 +0200

    Fixed the atmosphere creator.

--- lua/entities/ls_generator_atmosphere.lua
+++ lua/entities/ls_generator_atmosphere.lua
@@ -45,8 +45,8 @@ if SERVER then
 
 		if not self.active then
 
-			self.atmo = SThing.AddNewAtmosphere( self:GetPos(), 4/3*math.pi*1000^3*20 )
-			self.atmo:Set("oxygen", 4000)
+			self.atmo = SThing.AddNewAtmosphere( self:GetPos(), 1000 )
+			self.atmo:Set("oxygen", 4/3*math.pi*1000^3*20)
 			self.atmo:SetGravity(1)
 			self.active = true
 
